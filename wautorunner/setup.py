ddc713f59bb91fe2d3b24c591295fe7e06aa33d8
Dosclic98 <davidesavarro98@gmail.com>
2025-04-01 16:48:34 +0200

    Initial wallton-autorunner project setup

---
+++ wautorunner/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_namespace_packages, Command
+import os
+
+setup(
+    name='wautorunner',
+    version="0.1",
+    packages=find_namespace_packages(include=[
+        '*',
+        '*.__main__'
+    ]),
+    package_data={
+        '': ["*"]
+    },
+    entry_points={
+        "console_scripts": [
+            "wattson-autorunner = __main__:main"
+        ]
+    },
+    install_requires=[
+        "pyyaml>=5.4"
+    ],
+    python_requires=">=3.10.0",
+    author="Davide Savarro (Computer Science Department - University of Turin - UNITO)",
+    author_email="davide.savarro@unito.it",
+)
+
